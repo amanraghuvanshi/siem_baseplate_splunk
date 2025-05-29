26e0b34028ce68c643802dff880d6373735d0129
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2025-05-29 10:41:47 +0530

    db initiated

--- db/db.py
+++ db/db.py
@@ -2,3 +2,15 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config.settings import settings
 
+
+DATABASE_URL = settings.database_url
+
+engine = create_async_engine(DATABASE_URL, echo = True)
+
+AsyncSessionLocal = sessionmaker(bind = engine, class_ = AsyncSession, expire_on_commit = False)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
